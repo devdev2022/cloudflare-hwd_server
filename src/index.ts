4aa72537bf7641a6c5eca55dcc24626c5aa78fc9
khs <devdev0226@gmail.com>
2025-11-24 14:56:47 +0900

    Feat : 테스트서버 CORS 추가

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { getStaffPictures } from './controller/aboutUsController';
 import { getIntroduction, getSnsImg } from './controller/mainController';
 import { getPictures } from './controller/worksController';
 
-const allowedOrigins = ['https://hwayeondang.com'];
+const allowedOrigins = ['https://hwayeondang.com', 'https://43ceb531.hwd-44u.pages.dev'];
 
 function corsHeaders(origin: string) {
 	return {
